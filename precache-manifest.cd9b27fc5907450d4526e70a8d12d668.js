3f23d24863410d1a427b4f45be03802d3f70b301
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2018-11-08 18:47:11 -0500

    Updates

---
+++ precache-manifest.cd9b27fc5907450d4526e70a8d12d668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "143d840e8b5f237e35f0",
+    "url": "/react-spotifly/static/js/runtime~main.143d840e.js"
+  },
+  {
+    "revision": "f564d17aed2c5184869e",
+    "url": "/react-spotifly/static/js/main.f564d17a.chunk.js"
+  },
+  {
+    "revision": "e9784d2be73222230361",
+    "url": "/react-spotifly/static/js/1.e9784d2b.chunk.js"
+  },
+  {
+    "revision": "f564d17aed2c5184869e",
+    "url": "/react-spotifly/static/css/main.cc98f5ba.chunk.css"
+  },
+  {
+    "revision": "e9784d2be73222230361",
+    "url": "/react-spotifly/static/css/1.02848cc7.chunk.css"
+  },
+  {
+    "revision": "c5d453d02dd4236caf4d827cddafb9bc",
+    "url": "/react-spotifly/index.html"
+  }
+];
