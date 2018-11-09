e1cea30e713f328d30a5ffe0a2c9a4c5b0f1f333
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2018-11-08 19:17:05 -0500

    Updates

--- precache-manifest.04e5587496d0adddaf93e50ca42d8f30.js
+++ precache-manifest.04e5587496d0adddaf93e50ca42d8f30.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-spotifly/static/js/runtime~main.143d840e.js"
   },
   {
-    "revision": "1064f3e8c14a43f91de7",
-    "url": "/react-spotifly/static/js/main.1064f3e8.chunk.js"
+    "revision": "84ebb2d4531f0466f048",
+    "url": "/react-spotifly/static/js/main.84ebb2d4.chunk.js"
   },
   {
     "revision": "e9784d2be73222230361",
     "url": "/react-spotifly/static/js/1.e9784d2b.chunk.js"
   },
   {
-    "revision": "1064f3e8c14a43f91de7",
+    "revision": "84ebb2d4531f0466f048",
     "url": "/react-spotifly/static/css/main.cc98f5ba.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-spotifly/static/css/1.02848cc7.chunk.css"
   },
   {
-    "revision": "827901f1d38dc14f0ca4e97a2c22a6a9",
+    "revision": "e473f4664713517c5daa64b7f1e106c7",
     "url": "/react-spotifly/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-spotifly/precache-manifest.2a50b40908fe549287df234fffc28bad.js"
+  "/react-spotifly/precache-manifest.04e5587496d0adddaf93e50ca42d8f30.js"
 );
 
 workbox.clientsClaim();
