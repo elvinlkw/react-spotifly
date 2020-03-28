1f924f0fb8db572b67eec7d2cbe84e775fd6f4b8
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2020-03-28 13:01:34 -0400

    added logo on login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './../style/Login.css';
 import {client_id, scopes, redirect_url} from './../constants/app_cred.js';
+import logo from '../img/react-logo.png';
 
 class Login extends Component {
     render() { 
         return (
             <div className="container-login">
+                <img className="img-logo" src={logo} alt="Cannot be loaded"/>
                 <h1 id="react-app">Spotifly</h1>
                 <h5>Step 1:- Click on the button below to sign in and authorize your account to use the app.</h5>
                 <h5>Step 2:- That's it!</h5>
