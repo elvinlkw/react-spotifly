03f57a05d0e99c18353faf9f1e49f3fb908eb7bc
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2020-03-30 15:50:40 -0400

    Updates

--- precache-manifest.7c0a9ba2214a793bba45af53b417b94d.js
+++ precache-manifest.7c0a9ba2214a793bba45af53b417b94d.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/react-spotifly/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "6763a91a3f468171a0f8",
-    "url": "/react-spotifly/static/css/main.b0d3d899.chunk.css"
+    "revision": "a2cf3469cbacc67746eb",
+    "url": "/react-spotifly/static/css/main.1cfb4719.chunk.css"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
@@ -24,27 +24,27 @@ self.__precacheManifest = [
     "url": "/react-spotifly/static/media/react-logo.580ffd26.png"
   },
   {
-    "revision": "6763a91a3f468171a0f8",
-    "url": "/react-spotifly/static/js/main.6763a91a.chunk.js"
+    "revision": "a2cf3469cbacc67746eb",
+    "url": "/react-spotifly/static/js/main.a2cf3469.chunk.js"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/react-spotifly/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/react-spotifly/static/media/fontawesome-webfont.b06871f2.ttf"
-  },
-  {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/react-spotifly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-spotifly/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "4918db72179e6c756b4f",
     "url": "/react-spotifly/static/css/1.6178f7c7.chunk.css"
   },
   {
-    "revision": "7f66f546fff7ddb37cdf24b4d47d827d",
+    "revision": "f54a9f82f7ffaa77027768b33fd903a6",
     "url": "/react-spotifly/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-spotifly/precache-manifest.91d0165bab277c5f37e3bf9a0b42fee2.js"
+  "/react-spotifly/precache-manifest.7c0a9ba2214a793bba45af53b417b94d.js"
 );
 
 workbox.clientsClaim();
