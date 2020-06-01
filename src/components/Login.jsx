9c19b2287315194bde013750ea26dcd2872edb67
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2020-06-01 17:08:22 -0400

    converted into functional components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './../style/Login.css';
 import {client_id, scopes, redirect_url} from './../constants/app_cred.js';
 import logo from '../img/react-logo.png';
 
-class Login extends Component {
-    render() { 
-        return (
-            <div className="container-login">
-                <img className="img-logo" src={logo} alt="Cannot be loaded"/>
-                <h1 id="react-app">Spotifly</h1>
-                <h5>Step 1:- Click on the button below to sign in and authorize your account to use the app.</h5>
-                <h5>Step 2:- That's it!</h5>
-                <a href={`https://accounts.spotify.com/en/authorize/?client_id=${client_id}&response_type=token&redirect_uri=${redirect_url}&scope=${scopes}`}><button className="btn btn-success"><span>Sign In !</span></button></a>
-            </div>
-        );
-    }
+const Login = () => {
+    return (
+        <div className="container-login">
+            <img className="img-logo" src={logo} alt="Cannot be loaded"/>
+            <h1 id="react-app">Spotifly</h1>
+            <h5>Step 1:- Click on the button below to sign in and authorize your account to use the app.</h5>
+            <h5>Step 2:- That's it!</h5>
+            <a href={`https://accounts.spotify.com/en/authorize/?client_id=${client_id}&response_type=token&redirect_uri=${redirect_url}&scope=${scopes}`}><button className="btn btn-success"><span>Sign In !</span></button></a>
+        </div>
+    );
 }
 
 export default Login;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Homepage from './Homepage/Homepage';
 import Profile from './Profile/Profile';
@@ -6,18 +6,16 @@ import Search from './Search/Search';
 import TopTracks from './TopTracks/TopTracks';
 import TopArtists from './TopArtists/TopArtists';
 
-class Main extends Component {
-    render() { 
-        return (
-            <Switch>
-                <Route exact path="/" component={Homepage}></Route>
-                <Route exact path="/profile" component={Profile}></Route>
-                <Route exact path="/search" component={Search}></Route>
-                <Route exact path="/top-tracks" component={TopTracks}></Route>
-                <Route exact path="/top-artists" component={TopArtists}></Route>
-            </Switch>
-        );
-    }
+const Main = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Homepage}></Route>
+            <Route exact path="/profile" component={Profile}></Route>
+            <Route exact path="/search" component={Search}></Route>
+            <Route exact path="/top-tracks" component={TopTracks}></Route>
+            <Route exact path="/top-artists" component={TopArtists}></Route>
+        </Switch>
+    );
 }
 
 export default Main;
