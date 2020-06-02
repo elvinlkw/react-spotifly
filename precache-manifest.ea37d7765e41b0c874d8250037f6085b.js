f9c5e75efde8939f4209cc9c08ad0a270e38f682
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2020-06-01 22:59:25 -0400

    Updates

--- precache-manifest.ea37d7765e41b0c874d8250037f6085b.js
+++ precache-manifest.ea37d7765e41b0c874d8250037f6085b.js
@@ -4,16 +4,16 @@ self.__precacheManifest = [
     "url": "/react-spotifly/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "f31260e850520306f07f",
-    "url": "/react-spotifly/static/css/main.1cfb4719.chunk.css"
+    "revision": "c129ae668a19b0464736",
+    "url": "/react-spotifly/static/css/main.323bff13.chunk.css"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
     "url": "/react-spotifly/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "4918db72179e6c756b4f",
-    "url": "/react-spotifly/static/js/1.4918db72.chunk.js"
+    "revision": "ec09c8915e984563d935",
+    "url": "/react-spotifly/static/js/1.ec09c891.chunk.js"
   },
   {
     "revision": "143d840e8b5f237e35f0",
@@ -24,27 +24,27 @@ self.__precacheManifest = [
     "url": "/react-spotifly/static/media/react-logo.580ffd26.png"
   },
   {
-    "revision": "f31260e850520306f07f",
-    "url": "/react-spotifly/static/js/main.f31260e8.chunk.js"
+    "revision": "c129ae668a19b0464736",
+    "url": "/react-spotifly/static/js/main.c129ae66.chunk.js"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/react-spotifly/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/react-spotifly/static/media/fontawesome-webfont.674f50d2.eot"
-  },
-  {
     "revision": "b06871f281fee6b241d60582ae9369b9",
     "url": "/react-spotifly/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
-    "revision": "4918db72179e6c756b4f",
-    "url": "/react-spotifly/static/css/1.6178f7c7.chunk.css"
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-spotifly/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
-    "revision": "68294b6ffe3bebf7c18f8d3a6ab99269",
+    "revision": "ec09c8915e984563d935",
+    "url": "/react-spotifly/static/css/1.8d16a4f7.chunk.css"
+  },
+  {
+    "revision": "9fdcc5293fe0162c65c9d6d840458ce0",
     "url": "/react-spotifly/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-spotifly/precache-manifest.dce1d5fe193eef87811fb070049da094.js"
+  "/react-spotifly/precache-manifest.ea37d7765e41b0c874d8250037f6085b.js"
 );
 
 workbox.clientsClaim();
