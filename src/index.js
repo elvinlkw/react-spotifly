36393f8e07adf4c7577d974d0ee41e90e55b5b51
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2020-06-17 22:46:44 -0400

    added import for ToastProvider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {ToastProvider} from 'react-toast-notifications';
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.render(
-    <Router basename={process.env.PUBLIC_URL}>
-        <App/>
-    </Router>,
+    <ToastProvider>
+        <Router basename={process.env.PUBLIC_URL}>
+            <App/>
+        </Router>
+    </ToastProvider>,
     document.getElementById('root'));
 
