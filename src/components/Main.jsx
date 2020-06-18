497e3c5db799fc8b44d5b3fade4be4dacc2b433c
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2020-06-17 22:43:10 -0400

    converted into functional component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-import Landing from './Layout/Landing';
+import { Switch, Route } from 'react-router-dom';
+import Landing from './Landing/Landing';
 import Profile from './Profile/Profile';
 import Search from './Search/Search';
 import TopTracks from './TopTracks/TopTracks';
@@ -9,11 +9,11 @@ import TopArtists from './TopArtists/TopArtists';
 const Main = () => {
     return (
         <Switch>
-            <Route exact path="/" component={Landing}></Route>
-            <Route exact path="/profile" component={Profile}></Route>
-            <Route exact path="/search" component={Search}></Route>
-            <Route exact path="/top-tracks" component={TopTracks}></Route>
-            <Route exact path="/top-artists" component={TopArtists}></Route>
+            <Route exact path="/react-spotifly" component={Landing}></Route>
+            <Route exact path="/react-spotifly/profile" component={Profile}></Route>
+            <Route exact path="/react-spotifly/search" component={Search}></Route>
+            <Route exact path="/react-spotifly/top-tracks" component={TopTracks}></Route>
+            <Route exact path="/react-spotifly/top-artists" component={TopArtists}></Route>
         </Switch>
     );
 }
