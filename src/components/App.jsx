75e41991e1822e3e22e6e8de7fa8269b333ca157
ELVIN-DESKTOP\Elvin <elvinlkw@hotmail.com>
2020-08-05 21:04:19 -0400

    changed to hash router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Login from './Login';
 import Main from './Main';
 import Navbar from './Navbar';
